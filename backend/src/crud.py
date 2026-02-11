da7d89c743b1e7da100cd5de27bc88f03f411f15
Lucy <lu.anzoateguy@gmail.com>
2026-02-11 16:11:05 -0300

    Add user crud

---
+++ backend/src/crud.py
@@ -0,0 +1,20 @@
+from sqlalchemy.orm import Session
+from src import models, schemas
+from src.security import hash_password
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(models.User).filter(models.User.email == email).first()
+
+def create_user(db: Session, user: schemas.UserCreate):
+    hashed_pw = hash_password(user.password)
+
+    db_user = models.User(
+        username=user.username,
+        email=user.email,
+        hashed_password=hashed_pw
+    )
+
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
