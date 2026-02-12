4bf4d218f370382fef6347c04c4994e2a9dd05bb
Lucy <lu.anzoateguy@gmail.com>
2026-02-12 15:35:37 -0300

    Add frontend login/register interface

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from src.routes.auth import bp as auth_bp
+from flask_cors import CORS
 import os
 from src import models
 from src.database import engine, Base 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev_secret_key')
+CORS(app)
 
 with app.app_context():
     Base.metadata.create_all(bind=engine)
